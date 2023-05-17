484c63bb725d20ee5c654f539d8c669b930f4bda
GautamDhiman <gautamCodesHere@gmail.com>
2023-05-17 18:25:40 +0530

    Admin conf

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from rooms.models.room_inventory import RoomInventory
+
+
+@admin.register(RoomInventory)
+class AdminRoomInventory(admin.ModelAdmin):
+
+    list_per_page = 20
+    ordering = ['hotel_id']
+    list_display = ['hotel_id', 'room_id', 'date', 'available']
+    list_filter = ['hotel_id', 'room_id', 'date', 'available']
+
+    def has_delete_permission(self, request, obj=None):
+        """Removing delete permission."""
+        return False
