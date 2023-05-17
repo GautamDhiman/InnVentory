52cd2f101d91c4f18baa2196b3ef9abc94963da3
GautamDhiman <gautamCodesHere@gmail.com>
2023-05-17 14:33:18 +0530

    Configure MySql

--- InnVentory/settings.py
+++ InnVentory/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'InnVentory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'InnVentory',
+        'ENGINE': 'mysql.connector.django',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'GautamDhiman@10',
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
 
